b25c708f249fbaf96143aa09941145d7b5ed9a5e
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 21:27:43 -0300

    Adiciona repository Pedido

---
+++ src/main/java/com/bn/ex3/repositories/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.bn.ex3.repositories;
+
+import com.bn.ex3.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
