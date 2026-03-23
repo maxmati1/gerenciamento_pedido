3663a60bc23d46effde2e87b385ea2e3ba3e9fa0
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 21:26:27 -0300

    Adiciona entidade PedidoModel

---
+++ src/main/java/com/bn/ex3/models/PedidoModel.java
@@ -0,0 +1,52 @@
+package com.bn.ex3.models;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Pedido")
+@Table(name = "tb_pedido")
+public class PedidoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_pedido")
+    private LocalDateTime data;
+
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+
+    private String status;
+
+    public PedidoModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
